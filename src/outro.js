1d6614b7075f8e5d622f40f239f2277924b0f4e7
Taylor Zajicek <tzajicek@flexion.us>
2014-04-01 11:13:17 -0500

    RequireJS support

--- src/outro.js
+++ src/outro.js
@@ -1,2 +1,33 @@
 
+if (typeof define === 'function') {
+  define(function(){
+    return {
+      'BinaryOperator': BinaryOperator,
+      'CharCmds': CharCmds,
+      'Controller': Controller,
+      'Cursor': Cursor,
+      'Fragment': Fragment,
+      'LatexCmds': LatexCmds,
+      'Letter': Letter,
+      'MathBlock': MathBlock,
+      'MathCommand': MathCommand,
+      'MathElement': MathElement,
+      'MathQuill': MathQuill,
+      'Node': Node,
+      'NonSymbolaSymbol': NonSymbolaSymbol,
+      'PlusMinus': PlusMinus,
+      'Point': Point,
+      'RootMathBlock': RootMathBlock,
+      'RootMathCommand': RootMathCommand,
+      'RootTextBlock': RootTextBlock,
+      'Selection': Selection,
+      'Symbol': Symbol,
+      'TextBlock': TextBlock,
+      'TextPiece': TextPiece,
+      'VanillaSymbol': VanillaSymbol,
+      'Variable': Variable
+    };
+  });
+}
+
 }());
